26db26f535fd164f9e345df5d21663599c7927bc
YuliaDyka <handsmade.yulia@gmail.com>
2024-03-05 17:32:53 +0200

    updated home page

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:lab1/main.dart';
 
 class HomePage extends StatelessWidget {
-
   const HomePage({super.key});
-
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +15,6 @@ class HomePage extends StatelessWidget {
       ),
       home: const NavigationExample(),
     );
-
   }
 }
 
@@ -31,20 +28,6 @@ class NavigationExample extends StatefulWidget {
 class _NavigationExampleState extends State<NavigationExample> {
   int selectedIndex = 0;
 
-  // static const TextStyle optionStyle =
-  // TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  // static const List<Widget> widgetOptions = <Widget>[
-  //   Text(
-  //     'Index 0: Home',
-  //     style: optionStyle,
-  //   ),
-  //   Text(
-  //     'Index 1: Profile',
-  //     style: optionStyle,
-  //   ),
-  //
-  // ];
-
   void onItemTapped(int index) {
     setState(() {
       selectedIndex = index;
@@ -52,7 +35,7 @@ class _NavigationExampleState extends State<NavigationExample> {
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.lightGreen.shade300,
@@ -61,13 +44,15 @@ class _NavigationExampleState extends State<NavigationExample> {
           ElevatedButton(
             onPressed: () {
               Navigator.push(
-                  context,
-                  MaterialPageRoute<MyApp>(builder: (BuildContext context)
-                  => const MyApp(),),
-                );
+                context,
+                MaterialPageRoute<MyApp>(
+                  builder: (BuildContext context) => const MyApp(),
+                ),
+              );
             },
             style: ElevatedButton.styleFrom(
-              foregroundColor: Colors.white, backgroundColor: Colors.red[400],
+              foregroundColor: Colors.white,
+              backgroundColor: Colors.red[400],
             ),
             child: const Text('Logout'),
           ),
@@ -80,34 +65,236 @@ class _NavigationExampleState extends State<NavigationExample> {
       body: <Widget>[
         /// Home page
 
-               Column(
-                //mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  const Align(
-                    alignment: Alignment.centerLeft,
-                  //   child: ElevatedButton(
-                  //   child: const Text('Go back!'),
-                  //   onPressed: () {
-                  //     Navigator.push(
-                  //       context,
-                  //       MaterialPageRoute<MyApp>(builder: (BuildContext context)
-                  //       => const MyApp(),),
-                  //     );
-                  //   },
-                  // ),
-                  ),
-                  Expanded(
-                    child: Center(
-                      child: Text(
+        Column(
+          //mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Align(
+              alignment: Alignment.centerLeft,
+            ),
+            Expanded(
+              child: Center(
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 9,
+                    ),
+                    Text(
                       style: Theme.of(context).textTheme.headlineMedium,
                       textAlign: TextAlign.center,
                       'Home Page',
+                    ),
+                    Container(
+                      width: 400,
+                      height: 600,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(40),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(20),
+                        child: Column(
+                          children: [
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            const Text(
+                              'This is home page. Here will be all'
+                              ' information about your budget, your incomes '
+                              'and outcomes. Bellow is the pie-chart to see'
+                              ' your every day progress:',
+                              style: TextStyle(fontSize: 16),
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            const Icon(
+                              Icons.circle_outlined,
+                              size: 230,
+                            ),
+                            const SizedBox(
+                              height: 30,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                IconButton(icon: const Icon(Icons.arrow_back_ios_sharp),
+                                onPressed: (){},),
+                                IconButton(icon: const Icon(Icons.arrow_forward_ios_sharp),
+                                  onPressed: (){},),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(3),
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      color: Colors.lightGreen[300],
+                                    ),
+                                    padding: const EdgeInsets.symmetric(
+                                      vertical: 8,
+                                      horizontal: 14,
+                                    ),
+                                    child: const Column(
+                                      children: [
+                                        Text('7'),
+                                        Text('T'),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                Stack(
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.all(3),
+                                      child: Container(
+                                        decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.circular(10),
+                                          color: Colors.lightGreen,
+                                        ),
+                                        padding: const EdgeInsets.symmetric(
+                                          vertical: 8,
+                                          horizontal: 12,
+                                        ),
+                                        child: const Column(
+                                          children: [
+                                            Text('8'),
+                                            Text('F'),
+                                            Icon(Icons.attach_money_rounded),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                    Positioned(
+                                      right: 1,
+                                      top: 0,
+                                      child: Container(
+                                        width: 12,
+                                        height: 12,
+                                        decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.circular(20),
+                                          color: Colors.red,
+                                          border: Border.all(
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(3),
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      color: Colors.lightGreen[300],
+                                    ),
+                                    padding: const EdgeInsets.symmetric(
+                                      vertical: 8,
+                                      horizontal: 14,
+                                    ),
+                                    child: const Column(
+                                      children: [
+                                        Text('9'),
+                                        Text('S'),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(3),
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      color: Colors.lightGreen[300],
+                                    ),
+                                    padding: const EdgeInsets.symmetric(
+                                      vertical: 8,
+                                      horizontal: 12,
+                                    ),
+                                    child: const Column(
+                                      children: [
+                                        Text('10'),
+                                        Text('S'),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(3),
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      color: Colors.lightGreen[300],
+                                    ),
+                                    padding: const EdgeInsets.symmetric(
+                                      vertical: 8,
+                                      horizontal: 12,
+                                    ),
+                                    child: const Column(
+                                      children: [
+                                        Text('11'),
+                                        Text('M'),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(3),
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      color: Colors.lightGreen[300],
+                                    ),
+                                    padding: const EdgeInsets.symmetric(
+                                      vertical: 8,
+                                      horizontal: 12,
+                                    ),
+                                    child: const Column(
+                                      children: [
+                                        Text('12'),
+                                        Text('T'),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(3),
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      color: Colors.lightGreen[300],
+                                    ),
+                                    padding: const EdgeInsets.symmetric(
+                                      vertical: 8,
+                                      horizontal: 12,
+                                    ),
+                                    child: const Column(
+                                      children: [
+                                        Text('13'),
+                                        Text('W'),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            //),
+            ),
+          ],
+        ),
+        //),
 
         /// Profile page
         const Padding(
@@ -131,8 +318,6 @@ class _NavigationExampleState extends State<NavigationExample> {
             ],
           ),
         ),
-
-
       ][selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
@@ -140,18 +325,15 @@ class _NavigationExampleState extends State<NavigationExample> {
             icon: Icon(Icons.home),
             label: 'Home',
           ),
-
           BottomNavigationBarItem(
             icon: Icon(Icons.supervised_user_circle),
             label: 'Profile',
           ),
-
         ],
         currentIndex: selectedIndex,
         selectedItemColor: Colors.amber[800],
-       onTap: onItemTapped,
+        onTap: onItemTapped,
       ),
-
     );
   }
 }
